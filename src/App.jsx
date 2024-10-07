920a18f06d85e05c384f7f23d2072785a430889f
fullStackJourney <mdratul47782@example.com>
2024-10-07 11:17:34 +0600

    Initial commit for Real Estate Web Application

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,29 @@
 import AboutSection from "./Components/AboutSection";
-import FeaturesSection from "./Components/FeaturesSection";
-import HeroSection from "./Components/HeroSection";
-import Navbar from "./Components/Navbar";
-import PropertiesSection from "./Components/PropertiesSection";
+import Contact from "./Components/Contact";
 import CTASection from "./Components/CTASection";
 import FAQSection from "./Components/FAQSection";
-import Contact from "./Components/Contact";
+import FeaturesSection from "./Components/FeaturesSection";
 import FooterPart from "./Components/FooterPart";
-
+import HeroSection from "./Components/HeroSection";
+import Navbar from "./Components/Navbar";
+import OurTeamMembers from "./Components/OurTeamMembers";
+import PropertiesSection from "./Components/PropertiesSection";
 export default function App() {
   return (
     <>
+    
       <Navbar />
-      <HeroSection>
-        <FeaturesSection />
-        <PropertiesSection />
-        <AboutSection />
-        <CTASection />
-        <FAQSection />
-        <Contact />
-      </HeroSection>
+      <HeroSection/>
+      <FeaturesSection />
+      <PropertiesSection />
+      <AboutSection />
+      <CTASection />
+      <FAQSection />
+      <OurTeamMembers />
+      <Contact />
       <FooterPart />
+
+
     </>
   );
 }
